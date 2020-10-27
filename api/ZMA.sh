3ccf1bb4526a9e94666b513ccd22e7d0a7343d2b
Petr Šťastný <petr@soptik.tech>
2020-10-27 13:06:19 +0100

    Finished zma search

--- api/ZMA.sh
+++ api/ZMA.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 searchterm="$(base64 -d <<<"$1")"
 
-getPageTitle() {
-    fileAddr="$(awk '{print $2}' <<<"$1")"
-    #echo $fileAddr >&2
-    titlesSeparatedByColon="$(tr -d '\n' <"$fileAddr" | grep -Eo '<h3>.*</h3>' | sed 's/<\/span>/\n/g' | sed 's/.*<span>(.*)/\1/' -E | grep '\w{3,}' -E  | tr '\n' ':' | sed 's/.$/\n/')"
-    echo "$titlesSeparatedByColon"
-}
-
-
-
-
-result=$(grep "$searchterm" "../source/courses.fit.cvut.cz/BI-ZMA/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | fzf -f "$searchterm")
-echo "$result"
-exit 0
-files="$(echo "$result" | cut -d':' -f1 | sort | uniq -c | sort -nr)"
-IFS=$'\n'
-for file in $files; do
-    firstCol="$(awk '{print $1}' <<<"$file")"
-    secondCol="$(awk '{print $2}' <<<"$file")"
-    thirdCol="$(getPageTitle "$file" | base64)"
-    echo "$firstCol:${secondCol##*/}:$thirdCol"
-done
+result=$(grep "^" "../source/courses.fit.cvut.cz/BI-ZMA/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | fzf -f "$searchterm")
+files="$(echo "$result" | cut -d':' -f1 | sort | uniq -c | sort -nr | head -15 | sed 's/^\s*([^.]*)..\/source\/(.*)$/\1\2/' -E)"
+echo "$files"
 
