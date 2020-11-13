aa3203ebce39a55efce6da9d554293e7945dfa1c
Petr Šťastný <petr@soptik.tech>
2020-11-13 20:23:17 +0100

    Replaced fuzzy search with grep

--- api/ZMA.sh
+++ api/ZMA.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 searchterm="$(base64 -d <<<"$1")"
 
-result=$(grep "^" "../source/courses.fit.cvut.cz/BI-ZMA/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | fzf -f "$searchterm")
+result=$(grep "^" "../source/courses.fit.cvut.cz/BI-ZMA/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | grep -iF "$searchterm")
+#result=$(grep "^" "../source/courses.fit.cvut.cz/BI-ZMA/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | fzf -f "$searchterm")
 files="$(echo "$result" | cut -d':' -f1 | sort | uniq -c | sort -nr | head -15 | sed 's/^\s*([^.]*)..\/source\/(.*)$/\1\2/' -E)"
 echo "$files"
 
