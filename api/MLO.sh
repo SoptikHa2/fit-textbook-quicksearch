6ee8d477fabb1b0d4616c67c650f5c9313dafdc9
Petr Šťastný <petr@soptik.tech>
2020-12-02 16:26:33 +0100

    Hotfix: wrong iframe link MLO

--- api/MLO.sh
+++ api/MLO.sh
@@ -4,6 +4,6 @@ searchterm="$(base64 -d <<<"$1" | tr '[:lower:]' '[:upper:]')"
 searchterm_regexed="(${searchterm// /)|(})"
 result=$(grep "^" "../source/MLO/"*.txt | sort -k 2 | uniq --skip-fields=2 | grep -E "$searchterm_regexed")
 files="$(echo "$result" | cut -d':' -f1 | sort | uniq -c | sort -nr | head -15 | sed 's/^\s*([^.]*)..\/source\/(.*)$/\1\2/' -E)"
-urlified="$(echo "$files" | awk '{ sub(/.*-/, "", $2); sub(/\.txt$/, "", $2); print $1, "https://courses.fit.cvut.cz/BI-MLO/ucitele/trlifkat/mlo-skripta.pdf#page="$2; }')"
+urlified="$(echo "$files" | awk '{ sub(/.*-/, "", $2); sub(/\.txt$/, "", $2); print $1, "courses.fit.cvut.cz/BI-MLO/ucitele/trlifkat/mlo-skripta.pdf#page="$2; }')"
 echo "$urlified"
 
