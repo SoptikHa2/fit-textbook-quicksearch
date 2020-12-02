000ebd294b1a86c67c21471b68bb6f189e09e9f2
Petr Šťastný <petr@soptik.tech>
2020-12-02 11:09:29 +0100

    Moved ZMA reference to public KAM mirror

--- api/ZMA.sh
+++ api/ZMA.sh
@@ -2,7 +2,7 @@
 searchterm="$(base64 -d <<<"$1")"
 
 searchterm_regexed="(${searchterm// /)|(})"
-result=$(grep "^" "../source/courses.fit.cvut.cz/BI-ZMA/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | grep -iE "$searchterm_regexed")
+result=$(grep "^" "../source/kam.fit.cvut.cz/deploy/bi-zma/mirror/textbook/"*.html | sort -k 2 | uniq --skip-fields=2 | grep -iE "$searchterm_regexed")
 files="$(echo "$result" | cut -d':' -f1 | sort | uniq -c | sort -nr | head -15 | sed 's/^\s*([^.]*)..\/source\/(.*)$/\1\2/' -E)"
 echo "$files"
 
